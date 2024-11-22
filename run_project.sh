53038f57bd5a579b9beb0d612f4925f546767dab
PhylibhertBINGLEY <107876092+PhylibhertBINGLEY@users.noreply.github.com>
2024-11-22 23:34:47 +0100

    Analysing a single youtube channel

---
+++ run_project.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+# Active l'environnement virtuel
+source ~/Documents/Perso/Environnments/influencer_networks/Scripts/activate
+# Navigue dans le dossier du projet
+cd ~/Documents/Perso/Portfolios/mapping-ytb-influencer-networks
+# Exécute le script Python
+python script1.py
