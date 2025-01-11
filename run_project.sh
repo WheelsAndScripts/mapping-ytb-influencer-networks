26beea8de9a5d8321f64d3fa6417a56250e8d175
PhylibhertBINGLEY <107876092+PhylibhertBINGLEY@users.noreply.github.com>
2025-01-11 20:03:16 +0100

    Structure du .sh

--- run_project.sh
+++ run_project.sh
@@ -1,7 +1,23 @@
 #!/bin/bash
 # Active l'environnement virtuel
+echo "Activating Python virtual environment..."
 source ~/Documents/Perso/Environnments/influencer_networks/Scripts/activate
+
 # Navigue dans le dossier du projet
 cd ~/Documents/Perso/Portfolios/mapping-ytb-influencer-networks
-# Exécute le script Python
+
+# Exécute le script Python (test du shell)
+echo "Excuting the script"
 python script1.py
+
+# Étape 1 : Collecte des données via l'API YouTube
+
+# Étape 2 : Envoi des données dans Kafka (ingestion des données)
+
+# Étape 3 : Sauvegarde des données dans HDFS
+
+# Étape 4 : Transformation et traitement des données avec PySpark
+
+# Étape 5 : Construction du graphe avec PySpark GraphX ou NetworkX
+
+# Étape 6 : Génération de la visualisation
